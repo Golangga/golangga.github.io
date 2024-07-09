6e2a3a8d85e9d0672ca32db54f01afa6b1a961e1
golangga <gilangramadhanp226@gmail.com>
2024-07-09 19:25:01 +0700

    Perbaruan API

--- src/redux/actions/mahasiswaActions.js
+++ src/redux/actions/mahasiswaActions.js
@@ -4,7 +4,9 @@ export const fetchMahasiswa = () => async (dispatch) => {
   dispatch({ type: "FETCH_MAHASISWA_REQUEST" });
 
   try {
-    const response = await axios.get("http://localhost:5000/mahasiswa");
+    const response = await axios.get(
+      "https://668d29fd099db4c579f1f02c.mockapi.io/api/v1/mahasiswa"
+    );
     dispatch({ type: "FETCH_MAHASISWA_SUCCESS", payload: response.data });
   } catch (error) {
     dispatch({ type: "FETCH_MAHASISWA_FAILURE", payload: error.message });
